f08f55af55695531957a848ca2ac89dd987acc38
doc-bok <41854155+doc-bok@users.noreply.github.com>
2023-02-01 22:47:49 +0000

    Basic configuration and setup for the mod

---
+++ README.md
@@ -0,0 +1,7 @@
+# Butterflies
+ This mod adds butterflies to the world of Minecraft.
+ 
+## Changelog
+
+### [0.0.0] 2023-02-01
+ Basic configuration and setup for the mod.
--- src/main/java/com/bokmcdok/butterflies/ButterfliesMod.java
+++ src/main/java/com/bokmcdok/butterflies/ButterfliesMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.bokmcdok.butterflies;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -25,11 +25,11 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(ButterfliesMod.MODID)
+public class ButterfliesMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MODID = "examplemod";
+    public static final String MODID = "butterflies";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
@@ -42,7 +42,7 @@ public class ExampleMod
     // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
     public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
 
-    public ExampleMod()
+    public ButterfliesMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
