ba76d60dc3bec0eb9d726f554d36cd925640fac4
Doc Bok <docbok@dragonburn.org>
2024-06-28 12:09:29 +0700

    [FEATURE] Butterflies will now avoid other entities.

--- src/main/java/com/bokmcdok/butterflies/world/entity/animal/Butterfly.java
+++ src/main/java/com/bokmcdok/butterflies/world/entity/animal/Butterfly.java
@@ -22,15 +22,11 @@ import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.SpawnGroupData;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.control.FlyingMoveControl;
+import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
@@ -412,10 +408,9 @@ public class Butterfly extends Animal {
     protected void registerGoals() {
         super.registerGoals();
 
-        // TODO: Register a flee goal
-
+        this.goalSelector.addGoal(1, new AvoidEntityGoal<>(this, LivingEntity.class, 3, 0.8, 1.33, (x) -> !(x instanceof Butterfly)));
         this.goalSelector.addGoal(2, new ButterflyLayEggGoal(this, 0.8, 8, 8));
-        this.goalSelector.addGoal(3, new ButterflyMatingGoal(this, 1.1, 8));
+        this.goalSelector.addGoal(2, new ButterflyMatingGoal(this, 1.1, 8));
 
         // Pollination can be configured to be off.
         if (ButterfliesConfig.enablePollination.get()) {
