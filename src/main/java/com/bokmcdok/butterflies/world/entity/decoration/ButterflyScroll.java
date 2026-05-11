3053a5712161e528c36a17e91dedb77b46269602
Doc Bok <docbok@dragonburn.org>
2026-05-11 15:27:22 +0800

    [FIX] Remove unused import

--- src/main/java/com/bokmcdok/butterflies/world/entity/decoration/ButterflyScroll.java
+++ src/main/java/com/bokmcdok/butterflies/world/entity/decoration/ButterflyScroll.java
@@ -8,7 +8,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
