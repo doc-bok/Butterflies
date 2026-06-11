a96455811c870ad217cf13e58330ea95f2a09de8
Doc Bok <docbok@dragonburn.org>
2026-06-12 03:25:19 +0800

    [REFACTOR] Block states for bottled caterpillars and flower buds are now generated with datagen.

--- src/main/java/com/bokmcdok/butterflies/client/model/generators/ModBlockStateProvider.java
+++ src/main/java/com/bokmcdok/butterflies/client/model/generators/ModBlockStateProvider.java
@@ -2,30 +2,129 @@ package com.bokmcdok.butterflies.client.model.generators;
 
 import com.bokmcdok.butterflies.ButterfliesMod;
 import com.bokmcdok.butterflies.registries.BlockRegistry;
+import com.bokmcdok.butterflies.world.block.FlowerCropBlock;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * Registers block states.
+ */
 public class ModBlockStateProvider extends BlockStateProvider {
 
+    /**
+     * Construction.
+     * @param output The pack to output data to.
+     * @param existingFileHelper Helper to access existing files.
+     */
     public ModBlockStateProvider(PackOutput output,
-                                 ExistingFileHelper exFileHelper) {
-        super(output, ButterfliesMod.MOD_ID, exFileHelper);
+                                 ExistingFileHelper existingFileHelper) {
+        super(output, ButterfliesMod.MOD_ID, existingFileHelper);
     }
 
+    /**
+     * Entry point.
+     */
     @Override
     protected void registerStatesAndModels() {
-        registerBottledButterflies();
+
+        // Bottled items
+        for (int i = 0; i < BlockRegistry.BOTTLED_BUTTERFLY_BLOCKS.size(); i++) {
+            registerBottledButterfly(i);
+            registerBottledCaterpillar(i);
+        }
+
+        // Flower buds
+        registerFlowerBud(BlockRegistry.ALLIUM_BUD);
+        registerFlowerBud(BlockRegistry.AZURE_BLUET_BUD);
+        registerFlowerBud(BlockRegistry.BLUE_ORCHID_BUD);
+        registerFlowerBud(BlockRegistry.CORNFLOWER_BUD);
+        registerFlowerBud(BlockRegistry.DANDELION_BUD);
+        registerFlowerBud(BlockRegistry.LILY_OF_THE_VALLEY_BUD);
+        registerFlowerBud(BlockRegistry.ORANGE_TULIP_BUD);
+        registerFlowerBud(BlockRegistry.OXEYE_DAISY_BUD);
+        registerFlowerBud(BlockRegistry.PINK_TULIP_BUD);
+        registerFlowerBud(BlockRegistry.POPPY_BUD);
+        registerFlowerBud(BlockRegistry.RED_TULIP_BUD);
+        registerFlowerBud(BlockRegistry.WHITE_TULIP_BUD);
+        registerFlowerBud(BlockRegistry.WITHER_ROSE_BUD);
     }
 
-    private void registerBottledButterflies() {
+    /**
+     * Registers a bottled butterfly.
+     * @param index The butterfly index.
+     */
+    private void registerBottledButterfly(int index) {
+        registerBottledEntity(index, BlockRegistry.BOTTLED_BUTTERFLY_BLOCKS);
+    }
+
+    /**
+     * Registers a bottled caterpillar.
+     * @param index The butterfly index.
+     */
+    private void registerBottledCaterpillar(int index) {
+        registerBottledEntity(index, BlockRegistry.BOTTLED_CATERPILLAR_BLOCKS);
+    }
+
+    /**
+     * Registers a bottled entity.
+     * @param index The butterfly index.
+     * @param blocks The block list to register from.
+     */
+    private void registerBottledEntity(int index,
+                                       List<RegistryObject<Block>> blocks) {
         final ModelFile bottleModel = models().getExistingFile(new ResourceLocation(ButterfliesMod.MOD_ID, "bottle"));
-        for (RegistryObject<Block> block: BlockRegistry.BOTTLED_BUTTERFLY_BLOCKS) {
-            simpleBlock(block.get(), bottleModel);
+        simpleBlock(blocks.get(index).get(), bottleModel);
+    }
+
+    /**
+     * Registers a flower bud block state.
+     * @param block The block to create a block state for.
+     */
+    private void registerFlowerBud(RegistryObject<Block> block) {
+
+        if (block.get() instanceof FlowerCropBlock flowerBud) {
+
+            // This should never be null.
+            assert block.getId() != null;
+
+            String textureName = block.getId().getPath().substring(4);
+
+            Function<BlockState, ConfiguredModel[]> function =
+                    state -> generateCropState(state, flowerBud, textureName);
+
+            getVariantBuilder(flowerBud).forAllStates(function);
         }
+    }
+
+    /**
+     * Generates a block state variant for a specific age.
+     * @param state The current block state.
+     * @param block The block to generate.
+     * @param textureName The name of the texture to use.
+     * @return A new variant of the block state.
+     */
+    private ConfiguredModel[] generateCropState(BlockState state,
+                                                CropBlock block,
+                                                String textureName) {
+        ConfiguredModel[] models = new ConfiguredModel[1];
+        int age = Math.min(state.getValue(((FlowerCropBlock) block).getAgeProperty()), 6);
+        if (age < 5 && textureName.contains("tulip")) {
+            textureName = "tulip";
+        }
+
+        models[0] = new ConfiguredModel(models().getExistingFile(new ResourceLocation(ButterfliesMod.MOD_ID,
+                "block/flower_buds/" + textureName +"_stage" + age)));
+        return models;
     }
 }
