5b6925e174487ec1c0fb317c06dd32240b78e987
Doc Bok <docbok@dragonburn.org>
2026-06-12 17:02:41 +0800

    [REFACTOR] Butterfly nets are now generated using datagen.

--- src/main/java/com/bokmcdok/butterflies/client/model/generators/ModItemModelProvider.java
+++ src/main/java/com/bokmcdok/butterflies/client/model/generators/ModItemModelProvider.java
@@ -19,6 +19,8 @@ import java.util.Objects;
  */
 public class ModItemModelProvider extends ItemModelProvider {
 
+    private final static ResourceLocation HANDHELD_ROD = new ResourceLocation("item/handheld_rod");
+
     /**
      * Construction
      * @param output The pack to output the resources to.
@@ -36,9 +38,13 @@ public class ModItemModelProvider extends ItemModelProvider {
         for(int i = 0; i < ButterflyEntityTypeRegistry.BUTTERFLIES.size(); ++i) {
             registerBottledItems(i);
             registerButterflyEgg(i);
+            registerFullButterflyNet(i);
             registerButterflySpawnEggs(i);
         }
-        
+
+        registerSpecialButterflyNet(ItemRegistry.EMPTY_BUTTERFLY_NET);
+        registerSpecialButterflyNet(ItemRegistry.BURNT_BUTTERFLY_NET);
+
         basicItem(ItemRegistry.BUTTERFLY_BOOK.get());
     }
 
@@ -65,20 +71,37 @@ public class ModItemModelProvider extends ItemModelProvider {
      * @param index The butterfly index.
      */
     private void registerButterflySpawnEggs(int index) {
-        final ResourceLocation parent = new ResourceLocation("handheld_rod");
-        String path = Objects.requireNonNull(SpawnEggRegistry.BUTTERFLY_SPAWN_EGGS.get(index).getId()).getPath();
+        String path = getPath(SpawnEggRegistry.BUTTERFLY_SPAWN_EGGS.get(index));
         String species = path.substring(20);
 
-        singleTexture(path, parent, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/butterfly/" + species));
+        singleTexture(path, HANDHELD_ROD, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/butterfly/" + species));
 
-        path = Objects.requireNonNull(SpawnEggRegistry.CATERPILLAR_SPAWN_EGGS.get(index).getId()).getPath();
-        singleTexture(path, parent, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/caterpillar/" + species));
+        path = getPath(SpawnEggRegistry.CATERPILLAR_SPAWN_EGGS.get(index));
+        singleTexture(path, HANDHELD_ROD, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/caterpillar/" + species));
 
-        path = Objects.requireNonNull(SpawnEggRegistry.CHRYSALIS_SPAWN_EGGS.get(index).getId()).getPath();
-        singleTexture(path, parent, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/chrysalis/" + species));
+        path = getPath(SpawnEggRegistry.CHRYSALIS_SPAWN_EGGS.get(index));
+        singleTexture(path, HANDHELD_ROD, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/chrysalis/" + species));
 
-        path = Objects.requireNonNull(SpawnEggRegistry.EGG_SPAWN_EGGS.get(index).getId()).getPath();
-        singleTexture(path, parent, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/egg/" + species));
+        path = getPath(SpawnEggRegistry.EGG_SPAWN_EGGS.get(index));
+        singleTexture(path, HANDHELD_ROD, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/egg/" + species));
+    }
+
+    /**
+     * Registers a full butterfly net.
+     * @param index The butterfly index.
+     */
+    private void registerFullButterflyNet(int index) {
+        String path = getPath(ItemRegistry.BUTTERFLY_NETS.get(index));
+        singleTexture(path, HANDHELD_ROD, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/butterfly_net/butterfly_net_full"));
+    }
+
+    /**
+     * Registers a special butterfly net (e.g. burnt or empty).
+     * @param item The item to register.
+     */
+    private void registerSpecialButterflyNet(RegistryObject<Item> item) {
+        String path = getPath(item);
+        singleTexture(path, HANDHELD_ROD, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/butterfly_net/" + path));
     }
 
     /**
@@ -111,12 +134,18 @@ public class ModItemModelProvider extends ItemModelProvider {
                              String texturePrefix,
                              int substrStart)
     {
-        // The key should never be null.
-        assert item.getKey() != null;
-
-        ResourceLocation itemKey = item.getKey().location();
+        ResourceLocation itemKey = Objects.requireNonNull(item.getKey()).location();
         getBuilder(itemKey.toString())
                 .parent(new ModelFile.UncheckedModelFile("item/handheld_rod"))
                 .texture("layer0", new ResourceLocation(ButterfliesMod.MOD_ID,"item/" + textureLocation + "/" + texturePrefix + "_" + itemKey.getPath().substring(substrStart)));
+    }
+
+    /**
+     * Helper to get an item's path.
+     * @param item The item.
+     * @return The path of the item.
+     */
+    private String getPath(RegistryObject<Item> item) {
+        return Objects.requireNonNull(item.getId()).getPath();
     }
 }
