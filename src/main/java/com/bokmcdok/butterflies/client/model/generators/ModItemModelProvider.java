0e2a0b41c6da36e0b6b86b8a7ebe12f7a6218d01
Doc Bok <docbok@dragonburn.org>
2026-06-12 15:14:40 +0800

    [REFACTOR] Spawn eggs are now created using datagen.

--- src/main/java/com/bokmcdok/butterflies/client/model/generators/ModItemModelProvider.java
+++ src/main/java/com/bokmcdok/butterflies/client/model/generators/ModItemModelProvider.java
@@ -1,7 +1,9 @@
 package com.bokmcdok.butterflies.client.model.generators;
 
 import com.bokmcdok.butterflies.ButterfliesMod;
+import com.bokmcdok.butterflies.registries.ButterflyEntityTypeRegistry;
 import com.bokmcdok.butterflies.registries.ItemRegistry;
+import com.bokmcdok.butterflies.registries.SpawnEggRegistry;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
@@ -9,6 +11,8 @@ import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
+
+import java.util.Objects;
 
 /**
  * Generates Item Models for the mod.
@@ -29,16 +33,38 @@ public class ModItemModelProvider extends ItemModelProvider {
      */
     @Override
     protected void registerModels() {
-        registerBottledButterflies();
+        for(int i = 0; i < ButterflyEntityTypeRegistry.BUTTERFLIES.size(); ++i) {
+            registerBottledButterfly(i);
+            registerButterflySpawnEggs(i);
+        }
     }
 
     /**
      * Registers the bottled butterflies.
      */
-    private void registerBottledButterflies() {
-        for (RegistryObject<Item> item :ItemRegistry.BOTTLED_BUTTERFLIES) {
-            bottledButterflyItem(item);
-        }
+    private void registerBottledButterfly(int index) {
+        bottledButterflyItem(ItemRegistry.BOTTLED_BUTTERFLIES.get(index));
+    }
+
+    /**
+     * Register all buttery-related spawn eggs.
+     * @param index The butterfly index.
+     */
+    private void registerButterflySpawnEggs(int index) {
+        final ResourceLocation parent = new ResourceLocation("handheld_rod");
+        String path = Objects.requireNonNull(SpawnEggRegistry.BUTTERFLY_SPAWN_EGGS.get(index).getId()).getPath();
+        String species = path.substring(20);
+
+        singleTexture(path, parent, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/butterfly/" + species));
+
+        path = Objects.requireNonNull(SpawnEggRegistry.CATERPILLAR_SPAWN_EGGS.get(index).getId()).getPath();
+        singleTexture(path, parent, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/caterpillar/" + species));
+
+        path = Objects.requireNonNull(SpawnEggRegistry.CHRYSALIS_SPAWN_EGGS.get(index).getId()).getPath();
+        singleTexture(path, parent, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/chrysalis/" + species));
+
+        path = Objects.requireNonNull(SpawnEggRegistry.EGG_SPAWN_EGGS.get(index).getId()).getPath();
+        singleTexture(path, parent, "layer0", new ResourceLocation(ButterfliesMod.MOD_ID, "item/spawn_egg/egg/" + species));
     }
 
     /**
