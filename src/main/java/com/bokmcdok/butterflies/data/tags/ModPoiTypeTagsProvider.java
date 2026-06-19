56e46668f61a9684c9b675b24c8e66c3c8f2d790
Doc Bok <docbok@dragonburn.org>
2026-06-19 15:31:51 +0800

    [REFACTOR] Version bump.

--- src/main/java/com/bokmcdok/butterflies/data/tags/ModPoiTypeTagsProvider.java
+++ src/main/java/com/bokmcdok/butterflies/data/tags/ModPoiTypeTagsProvider.java
@@ -13,14 +13,27 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * Generates POI tags.
+ */
 public class ModPoiTypeTagsProvider extends PoiTypeTagsProvider {
 
+    /**
+     * Construction.
+     * @param packOutput The pack to output to.
+     * @param lookupProvider Helps with registry lookups.
+     * @param existingFileHelper Helps to check existing files.
+     */
     public ModPoiTypeTagsProvider(PackOutput packOutput,
                                   CompletableFuture<HolderLookup.Provider> lookupProvider,
                                   @Nullable ExistingFileHelper existingFileHelper) {
         super(packOutput, lookupProvider, ButterfliesMod.MOD_ID, existingFileHelper);
     }
 
+    /**
+     * Entry point.
+     * @param lookupProvider Helps with registry lookups.
+     */
     @Override
     protected void addTags(HolderLookup.@NotNull Provider lookupProvider) {
         tag(PoiTypeTags.ACQUIRABLE_JOB_SITE)
